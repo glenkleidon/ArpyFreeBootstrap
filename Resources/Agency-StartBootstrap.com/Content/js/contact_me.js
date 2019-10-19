0a02e71785eded7be6e4a01e2705295bc2e5d948
Glen Kleidon <glenk@galkam.com.au>
2019-10-19 22:47:00 +1100

    Updated Agency with new default contactme

--- Resources/Agency-StartBootstrap.com/Content/js/contact_me.js
+++ Resources/Agency-StartBootstrap.com/Content/js/contact_me.js
@@ -13,6 +13,7 @@ $(function() {
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
+      var token = $("input#formtoken").val();
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ');
@@ -23,6 +24,7 @@ $(function() {
         url: "././mail/contact_me.php",
         type: "POST",
         data: {
+          token: token,
           name: name,
           phone: phone,
           email: email,
@@ -40,13 +42,29 @@ $(function() {
             .append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
+        }, statusCode :
+        {
+           420: function () {
+          // Success message - already sent.
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message was already received.  It has not been resent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+
+           }
         },
         error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>")
+           .text("Sorry " + firstName + ", Your message was not accepted. Please Refresh the page or try again later!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
